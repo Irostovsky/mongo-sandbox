2b2292464da4012928b9e8fdccc73b2e3f36bd5c
Ivan Rostovsky <ir@irostovsky.(none)>
2012-10-26 13:24:19 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoSandbox::Application.config.secret_token = 'b9cf373ad4a980317b6e1ea783d016d801075dbd4fe60a60a05fc2ad239bb2cead523d81dc5d57a82d49d1b500ce485c422c052fdcac93a6d8a43db655048f04'
